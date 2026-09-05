b9bd64608662e1423b486a4d4f3d9840150f64ac
alopez9208 <alopezg9208@gmail.com>
2026-09-05 14:00:52 -0500

    Remove Firebase initialization and analytics
    Removed Firebase initialization and analytics code.

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBPyCYRfveDweaKMTISLQBi3tyaoEm88FM",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:1049503803358:web:cb9e17bde4c3dcf3905f4f",
   measurementId: "G-M7Z4NX4BVN"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
